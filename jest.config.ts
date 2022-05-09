55ee7cf168cba7d2c9d80f45249037ee90f449cd
Dominik Pieper <dominik@pieper.io>
2022-05-09 15:10:59 +0200

    feat(capacitor): add capacitor plugin

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,5 @@
 const { getJestProjects } = require('@nrwl/jest');
 
 module.exports = {
-  projects: [
-    ...getJestProjects(),
-    '<rootDir>/packages/tailwind',
-    '<rootDir>/e2e/tailwind-e2e',
-    '<rootDir>/e2e/sveltekit-e2e',
-    '<rootDir>/e2e/solid-e2e',
-    '<rootDir>/e2e/vite-e2e',
-  ],
+  projects: [...getJestProjects()],
 };
